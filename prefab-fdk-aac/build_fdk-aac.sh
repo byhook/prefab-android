d8cd321bd2bb51272beab2a3c6dfee98ed947ef2
byhook <byhook@163.com>
2023-11-01 17:09:34 +0800

    新增fdk-aac构建脚本

--- prefab-fdk-aac/build_fdk-aac.sh
+++ prefab-fdk-aac/build_fdk-aac.sh
@@ -3,16 +3,22 @@
 #参考文档
 #https://developer.android.com/ndk/guides/other_build_systems?hl=zh-cn#autoconf
 
+
 BUILD_DIR=$(pwd)/build
-SOURCE_CODE_DIR=$BUILD_DIR/lame-3.100
+SOURCE_CODE_DIR=$BUILD_DIR/source
 
 rm -rf $SOURCE_CODE_DIR
 mkdir -p $SOURCE_CODE_DIR
 
 #克隆代码到build目录下
-git clone git@github.com:open-source-mirrors/lame.git -b 3.100 $SOURCE_CODE_DIR
+git clone https://github.com/mstorsjo/fdk-aac $SOURCE_CODE_DIR
 
-cd $SOURCE_CODE_DIR
+#源代码依赖了android的日志库这里会报错
+#libSBRdec/src/lpp_tran.cpp:122:10: fatal error: 'log/log.h' file not found
+#解决方案：https://github.com/mstorsjo/fdk-aac/issues/124
+mkdir -p $SOURCE_CODE_DIR/libSBRdec/include/log/
+echo "void android_errorWriteLog(int i, const char *string){}" \
+  > $SOURCE_CODE_DIR/libSBRdec/include/log/log.h
 
 # 根据当前机器类型选择构建工具链
 export TOOLCHAIN=$NDK_ROOT/toolchains/llvm/prebuilt/darwin-x86_64
@@ -37,26 +43,31 @@ export LD=$TOOLCHAIN/bin/ld
 export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
 export STRIP=$TOOLCHAIN/bin/llvm-strip
 
-function build_lame {
+function build_library {
   ABI=$1
 
-  BUILD_DIR=$(pwd)/build/
+  PREFIX_DIR=$(pwd)/../build
+  mkdir -p $PREFIX_DIR
+
+  cd $SOURCE_CODE_DIR
+
+  autoreconf -fiv
 
   ./configure \
-    --prefix=$BUILD_DIR \
+    --prefix=$PREFIX_DIR \
     --host=$TARGET \
-    --bindir=$BUILD_DIR/bin \
-    --libdir=$BUILD_DIR/libs/$ABI \
+    --bindir=$PREFIX_DIR/bin \
+    --libdir=$PREFIX_DIR/libs/$ABI \
     --enable-shared=yes \
     --enable-static=yes
 
   #构建并安装
-  make install
+  make -j4 install
 }
 
-ABIS=("arm64-v8a" "armeabi-v7a" "x86_64" "x86")
+ABI_LIST=("arm64-v8a" "armeabi-v7a" "x86_64" "x86")
 
-for abi in ${ABIS[@]}; do
+for abi in ${ABI_LIST[@]}; do
   echo $abi
-  build_lame $abi
+  build_library $abi
 done
